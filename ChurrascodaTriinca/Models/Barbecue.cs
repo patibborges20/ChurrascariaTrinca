a66fb7f2bbebc403dc5288509cbb1f2d9e83bfae
Patricia Borges <patibborges@gmail.com>
2017-09-01 00:25:55 -0300

    ADD- Adicionando os pacotes iniciais e a modal principal(barbecue,participants)

---
+++ ChurrascodaTriinca/Models/Barbecue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ChurrascodaTriinca.Models
+{
+	//classe contendo o evento do churrasco
+	public class Barbecue
+    {
+            public int id{ get;set; }
+            public DateTime Date { get; set; }
+            public string Description { get; set; }
+            public double CostWithDrink { get; set; }
+            public double CostWhithoutDrink{ get; set; }
+    }
+}
---
+++ ChurrascodaTriinca/Models/Participant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ChurrascodaTriinca.Models
+{
+	//As informacoes dos participantes que participarao do churrasco
+	public class Participant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double ValueToPay { get; set; }
+        public bool Payment { get; set; }
+        bool withDrink { get; set; }
+    }
+}
